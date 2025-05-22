f21c6422b8b1a093c221cdeff84d372fa9fa9d76
inesuribeb <inesuribeb@gmail.com>
2025-05-22 16:45:54 +0200

    todos los links añadidos al modal about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,26 +1,3 @@
-// import './About.css'
-
-// function About({ isOpen, onClose }) {
-//     if (!isOpen) return null;
-    
-//     return (
-//         <div className='about-modal'>
-//             <button className='about-close-btn' onClick={onClose}>×</button>
-
-//             <div className='contact-links-to'>
-//                 <button className='email-c'>estudio@inesuribe.es</button>
-//                 <button className='instagram-c'>INSTAGRAM</button>
-//                 <button className='linkedin-c'>LINKEDIN</button>
-//                 <button className='direction-c'>BILBAO, BASQUE COUNTRY</button>
-
-//             </div>
-            
-//         </div>
-//     )
-// }
-
-// export default About;
-
 import './About.css'
 
 function About({ isOpen, onClose }) {
@@ -37,16 +14,32 @@ function About({ isOpen, onClose }) {
         // Evitar que el clic se propague al fondo
         e.stopPropagation();
     };
+
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:estudio@inesuribe.es';
+    };
+
+    const handleInstagramClick = () => {
+        window.open('https://www.instagram.com/estudio.inesuribe/', '_blank');
+    };
+
+    const handleLinkedInClick = () => {
+        window.open('https://www.linkedin.com/in/in%C3%A9s-uribe-barr%C3%B3n-325305143/', '_blank');
+    };
+
+    const handleLocationClick = () => {
+        window.open('https://www.google.com/maps/place/Bilbao,+Basque+Country,+Spain', '_blank');
+    };
     
     return (
         <div className='about-modal' onClick={handleBackgroundClick}>
             <button className='about-close-btn' onClick={onClose}>×</button>
 
             <div className='contact-links-to' onClick={handleButtonAreaClick}>
-                <button className='email-c'>estudio@inesuribe.es</button>
-                <button className='instagram-c'>INSTAGRAM</button>
-                <button className='linkedin-c'>LINKEDIN</button>
-                <button className='direction-c'>BILBAO, BASQUE COUNTRY</button>
+                <button className='email-c' onClick={handleEmailClick}>estudio@inesuribe.es</button>
+                <button className='instagram-c' onClick={handleInstagramClick}>INSTAGRAM</button>
+                <button className='linkedin-c' onClick={handleLinkedInClick}>LINKEDIN</button>
+                <button className='direction-c' onClick={handleLocationClick}>BILBAO, BASQUE COUNTRY</button>
             </div>
         </div>
     )
