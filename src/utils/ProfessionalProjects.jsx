fa18c53043fd7c105e3dcbfcec65348253e77dd0
inesuribeb <inesuribeb@gmail.com>
2025-05-22 20:30:18 +0200

    archivos con proyectos profesionales creado

---
+++ src/utils/ProfessionalProjects.jsx
@@ -0,0 +1,22 @@
+// ProfessionalProjects.jsx
+
+const professionalProjects = [
+    {
+        client: "INMO ARTAZA",
+        sector: {
+            es: ["Inmobiliario"],
+            en: ["Real Estate"]
+        },
+        services: {
+            es: ["Inmobiliario"],
+            en: ["Real Estate"]
+        },
+        web: "https://artaza.inesuribe.es/",
+        image: [
+            { src: "/Artaza/1.png", alt: "Captura INMO ARTAZA" },
+        ],
+        year: 2024,
+    },
+];
+
+export default professionalProjects;
