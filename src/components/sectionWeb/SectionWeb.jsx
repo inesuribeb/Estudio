0dfba33e4e233ddc8e8970626d6881054e8e9340
inesuribeb <inesuribeb@gmail.com>
2026-02-14 13:07:50 +0100

    fuente sans serif

--- src/components/sectionWeb/SectionWeb.jsx
+++ src/components/sectionWeb/SectionWeb.jsx
@@ -1,3 +1,50 @@
+// import Carousel from "../carousel/Carousel";
+// import './SectionWeb.css';
+// import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+
+// function SectionWeb({ title, web, description, method, link, images, className, language }) {
+//     const handleImageClick = () => {
+//         window.open(web, '_blank');
+//     };
+
+//     return (
+//         <section className={`web-section ${className}`}>
+        
+//             <div className="first-line">
+//                 <h3>
+//                     <a href={web} target="_blank" rel="noopener noreferrer">
+//                         # {title} <ArrowOutwardIcon />
+//                     </a>
+//                 </h3>
+//                 <p className="description">{description[language]}</p>
+//                 <p>{method}</p>
+//                 <p>
+//                     <a href={link} target="_blank" rel="noopener noreferrer">
+//                         <GitHubIcon />
+//                     </a>
+//                 </p>
+//             </div>
+//             <div className="second-line">
+//                 <Carousel>
+//                     {images.map((img, index) => (
+//                         <img
+//                             key={index}
+//                             src={img.src}
+//                             alt={img.alt}
+//                             loading="lazy"
+//                             onClick={handleImageClick}
+//                             style={{ cursor: 'pointer' }}
+//                         />
+//                     ))}
+//                 </Carousel>
+//             </div>
+//         </section>
+//     );
+// }
+
+// export default SectionWeb;
+
 import Carousel from "../carousel/Carousel";
 import './SectionWeb.css';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
@@ -12,19 +59,31 @@ function SectionWeb({ title, web, description, method, link, images, className, 
         <section className={`web-section ${className}`}>
         
             <div className="first-line">
-                <h3>
-                    <a href={web} target="_blank" rel="noopener noreferrer">
-                        # {title} <ArrowOutwardIcon />
-                    </a>
-                </h3>
-                <p className="description">{description[language]}</p>
-                <p>{method}</p>
-                <p>
-                    <a href={link} target="_blank" rel="noopener noreferrer">
-                        <GitHubIcon />
-                    </a>
-                </p>
+                <div className="title-container">
+                    <h3>
+                        <a href={web} target="_blank" rel="noopener noreferrer">
+                            # {title} <ArrowOutwardIcon />
+                        </a>
+                    </h3>
+                </div>
+                
+                <div className="description-container">
+                    <p className="description">{description[language]}</p>
+                </div>
+                
+                <div className="method-container">
+                    <p>{method}</p>
+                </div>
+                
+                <div className="github-container">
+                    <p>
+                        <a href={link} target="_blank" rel="noopener noreferrer">
+                            <GitHubIcon />
+                        </a>
+                    </p>
+                </div>
             </div>
+            
             <div className="second-line">
                 <Carousel>
                     {images.map((img, index) => (
